a62b1290e828e8efff5e12c6dc536ee1bef24bcf
scott-gianan <scott.gianan@gmail.com>
2022-10-09 14:14:02 +0800

    finished changing body color using DOM

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
+const btn = document.querySelector('button');
+const bodyColor = document.querySelector('body');
 
-
-function getRandomHex(){
+const getRandomHex = () => {
     let hexColor = '#';
     for (let i = 0; i < 6; i++) {
         hexColor += hex[Math.round(Math.random()*(hex.length-1))];
@@ -10,3 +11,9 @@ function getRandomHex(){
     //console.log(hexColor)
     return hexColor;
 }
+
+const changeColor = () => {
+    bodyColor.style.backgroundColor = getRandomHex();
+}
+
+btn.addEventListener("click", changeColor)
