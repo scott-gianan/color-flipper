f84c0a37a152d7ce1bec80a79007d64784d52d03
scott-gianan <scott.gianan@gmail.com>
2022-10-09 21:02:39 +0800

    added css, refactored js and added html elements. done

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 const btn = document.querySelector('button');
 const bodyColor = document.querySelector('body');
+const hexCodeText = document.querySelector('.hex-code-text');
 
 const getRandomHex = () => {
     let hexColor = '#';
     for (let i = 0; i < 6; i++) {
         hexColor += hex[Math.round(Math.random()*(hex.length-1))];
     }
-    //console.log(hexColor)
+    console.log(typeof hexColor)
     return hexColor;
 }
 
 const changeColor = () => {
     bodyColor.style.backgroundColor = getRandomHex();
+    hexCodeText.textContent = getRandomHex();
+    hexCodeText.style.color = getRandomHex();
 }
 
 btn.addEventListener("click", changeColor)
+
